a92184c4327117a9aa8741a35e44f4787480b01d
Riyakumari993191 <131858776+Riyakumari993191@users.noreply.github.com>
2024-01-18 10:56:39 +0000

    container config done

---
+++ datatype.js
@@ -0,0 +1,10 @@
+//number=>2to power 53
+//bigint
+//string =""
+//Boolean=true/false
+//null= standalone value
+//undefined=
+//Symbol= unique
+
+//object
+ console.log(typeof "riya");
---
+++ letvar.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountemail = "riyakumari@23"
+var accounntno = "1234567"
+acountcity ="jaypur"
+ //accountId = 45327
+ accountemail = "eiyakumari@123"    
+ accounntno = "23456"
+//console.log(accountId);
+//console.log(accountemail);
+console.table([accounntno,accountId,accountemail])
+
